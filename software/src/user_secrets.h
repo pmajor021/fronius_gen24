c13f7741754524573c6d29d92067ffd76a1864ea
pmajor021 <peter.majorhq@gmail.com>
2025-03-26 12:04:18 +0100

    initial release

---
+++ software/src/user_secrets.h
@@ -0,0 +1,7 @@
+#include "Arduino.h"
+//Name and password of Wifi network you want the emulator to connect to
+const char* WIFI_SSID = "MAJORhq";  
+const char* WIFI_PASSWORD = "Babos021Babos";
+
+//Fronius Inverter IP
+String Fronius_IP = "192.168.111.120";  // Fronius Gen24 Inverter IP address
